a2999c3dfac38c4079b71a61ab64df0bac78c56f
Ben Daniels <benbanj@hotmail.com>
2020-11-10 21:27:14 +0000

    Change page title

--- spec/requests/static_request_spec.rb
+++ spec/requests/static_request_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "Statics", type: :request do
   describe 'GET /' do
     it 'successfully loads homepage' do
       get root_path
-      byebug
       expect(response).to render_template(:homepage)
       expect(response).to have_http_status(200)
     end
