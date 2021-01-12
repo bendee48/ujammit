af794f0c034475117d12d6b78f190232720df314
Ben Daniels <benbanj@hotmail.com>
2021-01-12 21:41:36 +0000

    Adding #authorize, #request_token and #authorization code

--- app/lib/spotify_api.rb
+++ app/lib/spotify_api.rb
@@ -1,17 +1,32 @@
 module SpotifyApi
   AUTHORIZE_URL = 'https://accounts.spotify.com/authorize'.freeze
-  REDIRECT_URL  = 'http://localhost:3000'.freeze
+  TOKEN_URL = 'https://accounts.spotify.com/api/token'.freeze
+  REDIRECT_URL  = 'http://localhost:3000/callback'.freeze
+  CLIENT_ID = Rails.application.credentials.spotify[:client_id].freeze
+  CLIENT_SECRET = Rails.application.credentials.spotify[:client_secret].freeze
 
   def self.authorize
-    response = Faraday.get(AUTHORIZE_URL, { client_id:      Rails.application.credentials.spotify[:client_id],
-                                            response_type:  'code',
-                                            scope:          'user-read-private',
-                                            redirect_uri:   REDIRECT_URL,
-                                            state:          state })
+    Faraday.get(AUTHORIZE_URL, { client_id:      CLIENT_ID,
+                                 response_type:  'code',
+                                 scope:          'user-read-private',
+                                 redirect_uri:   REDIRECT_URL,
+                                 state:          state })
   end
 
   def self.state
     # code to protect against csrf
     SecureRandom.base64(12)
+  end
+
+  def self.request_token(auth_code)
+    # use authorization code and swap for access and refresh token
+    Faraday.post(TOKEN_URL) do |req|
+      req.body = { grant_type: 'authorization_code', code: auth_code, redirect_uri: REDIRECT_URL }
+      req.headers['Authorization'] = " Basic #{authorization_code}"
+    end
+  end
+
+  def self.authorization_code
+    Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")
   end
 end
