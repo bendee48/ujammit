5c5432e8f8e3d2ede97250285fbd1d8e96860e47
Ben Daniels <benbanj@hotmail.com>
2021-04-05 22:46:16 +0100

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Add Users
+User.create!(email: 'emma@email.com', password: 'password')
+puts "#{User.count} user#{'s' if User.count > 1} created."
