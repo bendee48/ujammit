d0e61195c97aae091cbf7164942bbcf0a4214b6b
Ben Daniels <benbanj@hotmail.com>
2021-05-13 17:06:23 +0100

    Adding Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'static#homepage'
   get '/authorize', to: 'spotify#authorize'
   get '/callback', to: 'spotify#callback'
+  resources :users, only: [:show]
 end
