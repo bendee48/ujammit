aacdcafec9900d46e073936d9679bf05fd31c97d
Ben Daniels <benbanj@hotmail.com>
2021-06-09 22:38:39 +0100

    Adding Authentication tests

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,6 +1,8 @@
 class SpotifyController < ApplicationController
   def authorize
     @response = SpotifyApi.authorize
+
+    # Redirect to #callback
     redirect_to @response.env.response_headers[:location]
   end
 
@@ -11,17 +13,22 @@ class SpotifyController < ApplicationController
     state = params[:state]
 
     @response = SpotifyApi.request_token(auth_code)
-
     @access_token = JSON.parse(@response.body)['access_token']
     @refresh_token = JSON.parse(@response.body)['refresh_token']
     @expires = JSON.parse(@response.body)['expires_in']
 
-    # Rails.cache.write(:access_token, @access_token, expires_in: @expires)
-    Rails.cache.write(:access_token, @access_token, expires_in: 15) # testing
-    Rails.cache.write(:refresh_token, @refresh_token)
+    save_tokens
 
     flash.notice = "Successfully authorised."
 
     redirect_to root_path
+  end
+
+  private
+
+  def save_tokens
+    # Rails.cache.write(:access_token, @access_token, expires_in: @expires)
+    Rails.cache.write(:access_token, @access_token, expires_in: 15) # testing
+    Rails.cache.write(:refresh_token, @refresh_token)
   end
 end
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentication', type: :feature do
-  let(:user) { build_stubbed(:user) }
+  let(:user) { create(:user) }
 
-  describe 'authorising Spotify' do
-    context 'user has not authorised Spotify' do
+  before(:each) do
+    Rails.cache.clear
+  end
+
+  describe 'authorizing Spotify' do
+    context 'user has not authorized Spotify' do
       xit 'does something' do
         login_as(user, scope: :user)
 
@@ -14,29 +18,51 @@ RSpec.describe 'Authentication', type: :feature do
         expect(Rails.cache.exist?(:access_token)).to eql false
       end
 
-      it 'shows button for authorisation' do
+      it 'shows button for authorization' do
         login_as(user, scope: :user)
 
         visit root_path
 
         expect(page).to have_current_path(root_path)
-        expect(page).to have_selector('#authorise-btn')
+        expect(page).to have_selector('#authorize-btn')
       end
     end
 
-    context 'user has authorised Spotify' do
-      it 'does not show authorise button', driver: :mechanize do
-        # will have to stub response and keys so testing whther button is there?      
+    context 'user has authorized Spotify' do
+      it 'does not show authorize button', driver: :mechanize do
         login_as(user, scope: :user)
         visit root_path
 
-        click_on('Authorize Spotify')
-        #signs out here, not sure why using mechanize so calls to outside should work# 
+        # authorize
+        allow(SpotifyApi).to receive(:state).and_return('xyxyx')
+        stub_const("#{SpotifyApi}::CLIENT_ID", '12345')
+        
+        stub_request(:get, SpotifyApi::AUTHORIZE_URL)
+          .with(query: { client_id: SpotifyApi::CLIENT_ID,
+                        response_type: 'code',
+                        scope: 'user-read-recently-played',
+                        redirect_uri: SpotifyApi::REDIRECT_URL,
+                        state: 'xyxyx' })
+          .to_return(headers: {location: callback_path})
 
-        # need to stub redirect maybe?? currently logs user u
+        #callback
+        credentials = 'xyxyxyx'
+        auth_code = nil
+        allow(SpotifyApi).to receive(:encoded_credentials).and_return(credentials)
+
+        stub_request(:post, SpotifyApi::TOKEN_URL)
+          .with(body: { grant_type: 'authorization_code',
+                      code: auth_code,
+                      redirect_uri: SpotifyApi::REDIRECT_URL,
+                      },
+                headers: { authorization: "Basic #{SpotifyApi.encoded_credentials}" })
+          .to_return(body: {access_token: '1234', refresh_token: '5678', expires_in: 15}.to_json )
+          
+        click_on('Authorize Spotify')
 
         expect(page).to have_current_path(root_path)
-        expect(page).to_not have_selector('#authorise-btn')
+        expect(page).to_not have_selector('#authorize-btn')
+        expect(Rails.cache.fetch(:refresh_token)).to eql '5678'
       end
     end
   end
