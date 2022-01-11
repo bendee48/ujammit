f2697f81f30da1adf68a10fafcc7a5f5bf88f897
Ben Daniels <benbanj@hotmail.com>
2022-01-11 21:31:24 +0000

    Add check for presence of access token in #access_valid? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   encrypts  :access_token, :refresh_token
 
   def access_valid?
-    access_token_expiration > DateTime.now
+    access_token_expiration > DateTime.now if access_token.present?
   end
 end
